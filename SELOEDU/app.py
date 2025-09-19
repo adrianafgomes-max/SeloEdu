d619f50d16072d4cc15c51b9d83dfb7d43455485
adrianafgomes-max <adriana.fgomes@souunit.com.br>
2025-09-18 21:23:37 -0300

    Add files via upload

---
+++ SELOEDU/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/login")
+def login():
+    return render_template("auth/login.html")
+
+@app.route("/do_login", methods=["POST"])
+def do_login():
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
